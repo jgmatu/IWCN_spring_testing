365c24f45b0a8e80ae213459922ad2d736582e5e
Javi <fj.gutierrezs91@gmail.com>
2017-10-15 23:01:37 +0200

    Languages feature added

--- src/main/java/es/urjc/javsan/master/Application.java
+++ src/main/java/es/urjc/javsan/master/Application.java
@@ -1,21 +1,59 @@
 package es.urjc.javsan.master;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import es.urjc.javsan.services.ProductService;
 
 @SpringBootApplication
 @ComponentScan("es.urjc.javsan.controllers")
-public class Application {
+public class Application extends WebMvcConfigurerAdapter {
 
 	@Bean
 	public ProductService productService() {
 		return new ProductService();
 	}
 
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource msgSrc = new ResourceBundleMessageSource();
+		
+		msgSrc.setBasename("messages");
+		return msgSrc;
+	}
+	
+	@Bean
+	public SessionLocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		
+		sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+		return sessionLocaleResolver;
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		super.addInterceptors(registry);
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+	
+	@Bean 
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor result = new LocaleChangeInterceptor();
+
+		result.setParamName("lang");
+		return result;
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
