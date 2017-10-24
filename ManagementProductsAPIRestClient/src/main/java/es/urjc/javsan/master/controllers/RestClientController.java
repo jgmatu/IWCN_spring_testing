efb87af76724b9fb4dd1b7266872fe830568d31e
Javi <fj.gutierrezs91@gmail.com>
2017-10-24 21:13:24 +0200

    Happy path implemented :)

--- ManagementProductsAPIRestClient/src/main/java/es/urjc/javsan/master/controllers/RestClientController.java
+++ ManagementProductsAPIRestClient/src/main/java/es/urjc/javsan/master/controllers/RestClientController.java
@@ -2,7 +2,8 @@ package es.urjc.javsan.master.controllers;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,72 +12,81 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
-import es.urjc.javsan.master.configuration.DatabaseLoader;
 import es.urjc.javsan.master.entities.Product;
 
-@Controller("/")
-public class DataBaseController {
+@Controller
+public class RestClientController {
 		
 	private static final String REST = "http://localhost:8080";
 	
-	@Autowired
-	private DatabaseLoader productDatabase;
-
 	@RequestMapping("/")
 	public ModelAndView greeting() {				
 		return new ModelAndView("greeting_template");
 	}
 
 	@GetMapping("/add") 
-	public ModelAndView edit(Product product) {
+	public ModelAndView add(Product product) {
 		return new ModelAndView("form_product");
 	}
 	
 	@PostMapping("/add")
-    public ModelAndView addSubmit(@Valid Product product, BindingResult bindingResult) {
+    public ModelAndView add(@Valid Product product, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			return new ModelAndView("form_product");
 		}
 		
-		// API Client add.
 		RestTemplate restTemplate = new RestTemplate();
 		String url = REST + "/add";
 		String response = restTemplate.postForObject(url, product, String.class);
-		System.out.println(response);
+	
 		return new ModelAndView("greeting_template");
     }
 
 	@GetMapping("/edit") 
-	public ModelAndView add(@RequestParam int code) {
-		return new ModelAndView("form_edit")
-				.addObject("product", productDatabase.get(code));
-	}
+	public ModelAndView edit(@RequestParam int code) {
+		RestTemplate restTemplate = new RestTemplate();
+		String url = REST + "/product?code="+ String.valueOf(code);
+		Product response = restTemplate.getForObject(url, Product.class);
 
+		return new ModelAndView("form_edit").addObject("product", response);
+	}
 	
 	@PostMapping("/edit")
     public ModelAndView editSubmit(@Valid Product product, BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			return new ModelAndView("form_edit");
 		}	
-		productDatabase.edit(product);
+		RestTemplate restTemplate = new RestTemplate();
+		String url = REST + "/edit";
+		String response = restTemplate.postForObject(url, product, String.class);
+	
 		return new ModelAndView("greeting_template");
     }
 		
 	@RequestMapping("/list")
 	public ModelAndView list() {
-		return new ModelAndView("list_products")
-				.addObject("productService", productDatabase.findAll());		
+		RestTemplate restTemplate = new RestTemplate();
+		String url = REST + "/list";
+		ResponseEntity<Product[]> responseEntity = restTemplate.getForEntity(url, Product[].class);
+		Product[] products = responseEntity.getBody();
+		
+		return new ModelAndView("list_products").addObject("productService", products);		
 	}
 	
 	@RequestMapping("/delete")
 	public ModelAndView delete(@RequestParam int code) {
-		productDatabase.delete(code);
+		RestTemplate restTemplate = new RestTemplate();
+		
+		restTemplate.delete(REST + "/delete?code=" + String.valueOf(code));
 		return new ModelAndView("greeting_template");
 	}
 	
 	@RequestMapping("/product")
 	public ModelAndView product(@RequestParam int code) {
-		return new ModelAndView("product")
-				.addObject("product", productDatabase.get(code));		
+		RestTemplate restTemplate = new RestTemplate();
+		String url = REST + "/product?code=" + String.valueOf(code);
+		Product product = restTemplate.getForObject(url, Product.class);
+		
+		return new ModelAndView("product").addObject("product", product);		
 	}
 }
